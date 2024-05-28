adb56286536af20451d76f3ec6c2f8af1f0547c7
Kaz Piekos <python_js@icloud.com>
2024-05-28 11:30:55 +0100

    fifth final

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,6 +4,10 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const db = globalThis.prisma || new PrismaClient();
+export let db = globalThis.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = db;
+} else {
+  db = new PrismaClient();
+}
